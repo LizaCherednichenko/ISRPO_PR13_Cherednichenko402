516397cde9132d631e18dd1eec50f43300c2b9ae
elizavetacher44@gmail.com <elizavetacher44@gmail.com>
2022-11-12 10:37:15 +1000

    Добавьте файлы проекта.

---
+++ ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISRPO_PR13_Cherednichenko402.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static Sklad model0db;
+    }
+}
---
+++ ApplicationData/AppFrame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ISRPO_PR13_Cherednichenko402.ApplicationData
+{
+    internal class AppFrame
+    {
+        public static Frame MyFrame;
+    }
+}
